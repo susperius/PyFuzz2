6db46a1cf1e6ed896addce8749e90abe8bdfc39c
susperius <susperius@gmail.com>
2015-04-26 11:52:40 +0200

    implemented a ConfigParser class to remove all config stuff from Node TODO: Change ByteMut fuzzer to hold the file data in memory and add functionality for iterations

---
+++ PyFuzz2-Node/model/config.py
@@ -0,0 +1,68 @@
+__author__ = 'susperius'
+
+import xml.etree.ElementTree as ET
+import logging
+
+
+class ConfigParser:
+    def __init__(self, config_filename):
+        self._logger = logging.getLogger(__name__)
+        try:
+            self._tree = ET.parse(config_filename)
+            self._root = self._tree.getroot()
+            self._node_name = self._root.attrib['name']
+            self._node_mode = self._root.attrib['mode']
+            if self._node_mode == "net":
+                beacon = self._root.find("beacon")
+                reporting = self._root.find("reporting")
+                listener = self._root.find("listener")
+                self._beacon_server = beacon.attrib['server']
+                self._beacon_port = int(beacon.attrib['port'])
+                self._beacon_interval = int(beacon.attrib['interval'])
+                self._report_server = reporting.attrib['server']
+                self._report_port = reporting.attrib['port']
+                self._listener_port = int(listener.attrib['port'])
+            elif self._node_mode != "single":
+                raise ValueError("Only net and single are available modes for node!")
+            fuzzer = self._root.find("fuzzer")
+            self._fuzzer_type = fuzzer.attrib['type']
+            self._fuzz_config = []
+            if self._fuzzer_type == "bytemutation":
+                self._fuzz_config.append(fuzzer.attrib['min_change'])
+                self._fuzz_config.append(fuzzer.attrib['max_change'])
+                self._fuzz_config.append(fuzzer.attrib['seed'])
+                self._fuzz_config.append(fuzzer.attrib['fuzz_file'])
+            elif self._fuzzer_type == "js_fuzzer":
+                self._fuzz_config.append(fuzzer.attrib['starting_elements'])
+                self._fuzz_config.append(fuzzer.attrib['total_operations'])
+                self._fuzz_config.append(fuzzer.attrib['browser'])
+                self._fuzz_config.append(fuzzer.attrib['seed'])
+            else:
+                raise ValueError("Unsupported fuzzer type!")
+        except Exception as ex:
+            self._logger.error("General error occurred while parsing config: " + ex.message)
+            quit()
+
+    @property
+    def node_name(self):
+        return self._node_name
+
+    @property
+    def node_mode(self):
+        return self._node_mode
+
+    @property
+    def beacon_config(self):
+        return self._beacon_server, self._beacon_port, self._beacon_interval if self._node_mode == "net" else None
+
+    @property
+    def report_config(self):
+        return self._report_server, self._report_port if self._node_mode == "net" else None
+
+    @property
+    def listener_config(self):
+        return self._listener_port if self._node_mode == "net" else None
+
+    @property
+    def fuzzer_config(self):
+        return self._fuzzer_type, self._fuzz_config
--- PyFuzz2-Node/pyfuzz2_node.py
+++ PyFuzz2-Node/pyfuzz2_node.py
@@ -10,6 +10,7 @@ from communication.tcplistener import Listener
 from gevent.queue import Queue
 from fuzzer.fuzzers import FUZZERS
 from worker.listenerworker import ListenerWorker
+from model.config import ConfigParser
 
 gevent.monkey.patch_all()
 
@@ -20,47 +21,19 @@ pyfuzznode = None
 class PyFuzz2Node:
     def __init__(self, logger, config_filename=CONFIG_FILENAME):
         self._logger = logger
-        self._node_name = ""
-        self._beacon_server = ""
-        self._beacon_port = 0
-        self._beacon_interval = 0
-        self._tcp_listener_port = 0
-        self._fuzzer = None
-        self._fuzz_file = ""
-        self._read_config(config_filename)
+        node_config = ConfigParser(config_filename)
+        self._node_name = node_config.node_name
+        self._node_mode = node_config.node_mode
+        if self._node_mode == "net":
+            self._beacon_server, self._beacon_port, self._beacon_interval = node_config.beacon_config
+            self._report_server, self._report_port = node_config.report_config
+            self._tcp_listener_port = node_config.listener_config
+        self._fuzzer_type, self._fuzzer_config = node_config._fuzz_config
         self._listener_queue = Queue()
         self._beacon_client = BeaconClient(self._beacon_server, self._beacon_port, self._node_name,
                                            self._beacon_interval, self._tcp_listener_port)
         self._tcp_listener = Listener(self._tcp_listener_port, self._listener_queue)
         self._listener_worker = ListenerWorker(self._listener_queue)
-
-    def _read_config(self, config_filename):
-        tree = ET.parse(config_filename)
-        root = tree.getroot()
-        beacon = root.find("beacon")
-        listener = root.find("listener")
-        fuzzer_conf = root.find("fuzzer")
-        self._node_name = root.attrib['name']
-        self._beacon_server = beacon.attrib['server']
-        self._beacon_port = int(beacon.attrib['port'])
-        self._beacon_interval = int(beacon.attrib['interval'])
-        self._tcp_listener_port = int(listener.attrib['port'])
-        self._fuzzer = self._choose_fuzzer(fuzzer_conf)
-
-    def _choose_fuzzer(self, fuzzer_conf):
-        f_type = fuzzer_conf.attrib['type']
-        if f_type in FUZZERS:
-            if f_type == "bytemutation":
-                from fuzzer.bytemutation import ByteMutation
-                min_change = int(fuzzer_conf.attrib['min_change'])
-                max_change = int(fuzzer_conf.attrib['max_change'])
-                seed = int(fuzzer_conf.attrib['seed'])
-                iteration = int(fuzzer_conf.attrib['iteration'])
-                self._fuzz_file = fuzzer_conf.attrib['fuzz_file']
-                return ByteMutation(min_change, max_change, seed, iteration)
-        else:
-            raise Exception("No such fuzzer!")
-        pass
 
     def main(self):
         self._logger.info("PyFuzz 2 Node started ...")

ef2e3638b05ce71a7c7a3709379309cde2306b67
susperius <susperius@gmail.com>
2015-07-03 16:10:58 +0200

    fixing again

--- PyFuzz2-Node/fuzzing/jsfuzzer/domObjects.py
+++ PyFuzz2-Node/fuzzing/jsfuzzer/domObjects.py
@@ -70,14 +70,13 @@ class DomObjects:
     DOM_EVENTS_TRANSITION = ['transitionend']
     DOM_EVENTS_MISC = ['onmessage', 'onmousewheel', 'ononline', 'onoffline', 'onpopstate', 'onshow', 'onstoreage',
                        'ontoggle', 'onwheel']
-    DOM_EVENTS_USABLE = ['click', 'error', 'load', 'scroll', 'resize', 'change', 'focus', 'focusin', 'blur', 'select',
-                         'pageshow', 'unload', 'beforeunload']
+    DOM_EVENTS_USABLE = ['click', 'error', 'load', 'scroll', 'resize', 'change', 'focus', 'focusin', 'blur', 'select']
 
     DOM_MUTATION_EVENTS = ['DOMAttrModified', 'DOMAttributeNameChanged', 'DOMCharacterDataModified',
                            'DOMElementNameChanged', 'DOMNodeInserted', 'DOMNodeRemoved', 'DOMNodeRemoved',
                            'DOMNodeRemovedFromDocument', 'DOMSubtreeModified']
 
-    DOM_EVENTS = DOM_EVENTS_USABLE #+ DOM_MUTATION_EVENTS
+    DOM_EVENTS = DOM_EVENTS_USABLE + DOM_MUTATION_EVENTS
     # END DOM EVENTS
 
     # START DOM EVENT OBJECT

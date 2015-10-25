e091733b8c3f183cd22718d049029aa684633d37
susperius <susperius@gmail.com>
2015-10-25 10:46:57 +0100

    little tweak to avoid the node to crash if the server isn't reachable

--- node/communication/reportclient.py
+++ node/communication/reportclient.py
@@ -4,12 +4,17 @@ import gevent
 import gevent.socket
 import os
 
+
 class ReportClient:
     def __init__(self, report_server, report_server_port):
         self._server = report_server
         self._port = report_server_port
 
     def send(self, data):
-        sock = gevent.socket.create_connection((self._server, self._port))
-        sock.send(data)
-        sock.close()
+        try:
+            sock = gevent.socket.create_connection((self._server, self._port))
+            sock.send(data)
+            sock.close()
+        except IOError:
+            pass  # TODO: think about a more reliable solution ... this is just to be sure the reportworker isn't
+                  # is not crashing and to keep the node working ....

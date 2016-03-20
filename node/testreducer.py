300dbe54bb24ec4d635bc20a91dd54cc3ef51e1e
Martin Sablotny <susperius@mac-mobile.local>
2016-03-20 10:32:45 +0100

    trying different methods to reduce html

--- node/testreducer.py
+++ node/testreducer.py
@@ -1,26 +1,63 @@
 import reducing.javascript
+import logging
 
+formatter = logging.Formatter("%(levelname)s: %(message)s")
+logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
 
 red = reducing.javascript.JsReducer("html")
 
-red.set_case("./", "reduced2.html")
+red.set_case("./", "test56.html")
 
-# case = red.test()
+print(red.test())
 
-
+"""
 case = red.reduce()
 red.crashed(False)
 
 
 case = red.reduce()
 red.crashed(True)
-"""
-case = red.reduce()
-red.crashed(True)
-
+print(len(case))
 
 case = red.reduce()
 red.crashed(True)
+print(len(case))
+
+case = red.reduce()
+red.crashed(True)
+print(len(case))
+
+case = red.reduce()
+red.crashed(True)
+print(len(case))
+
+case = red.reduce()
+red.crashed(True)
+print(len(case))
+
+case = red.reduce()
+red.crashed(True)
+print(len(case))
+
+case = red.reduce()
+red.crashed(True)
+print(len(case))
+
+case = red.reduce()
+red.crashed(True)
+print(len(case))
+
+case = red.reduce()
+red.crashed(True)
+print(len(case))
+
+while case is not None:
+    case = red.reduce()
+    red.crashed(True)
+    print(len(case))
+
 case = red.reduce()
 red.crashed(True)
 case = red.reduce()
@@ -33,9 +70,10 @@ case = red.reduce()
 red.crashed(True)
 case = red.reduce()
 red.crashed(True)
-"""
+
 
 with open("red00.html", 'wb+') as fd:
     fd.write(case)
     red.crashed(True)
 
+"""
